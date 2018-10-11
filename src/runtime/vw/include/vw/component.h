1b269166993115dc8a5b1753a72dfcfd74a0033e
1589221904@qq.com <xuesheng.zhang>
2018-10-12 00:34:23 +0800

    调整一下文件位置

--- src/runtime/vw/include/vw/component.h
+++ src/runtime/vw/include/vw/component.h
@@ -2,11 +2,13 @@
 #define COMPONENT_H
 
 #include "vw_global.h"
+#include <QObject>
 
 namespace vw {
 
-class VW_DECL Component
+class VW_DECL Component : public QObject
 {
+    Q_OBJECT
 public:
     Component();
 };
--- src/runtime/vw/include/vw/entity.h
+++ src/runtime/vw/include/vw/entity.h
@@ -2,13 +2,25 @@
 #define ENTITY_H
 
 #include "vw_global.h"
+#include <QObject>
+#include <QList>
+#include "component.h"
 
 namespace vw {
 
-class VW_DECL Entity
+class VW_DECL Entity : public QObject
 {
+    Q_OBJECT
 public:
     Entity();
+
+    void addChild(Entity *entity);
+    void removeChild(Entity *entity);
+    QList<Entity *> entities();
+
+    void addComponent(Component *component);
+    void removeComponent(Component *component);
+    QList<Component *> components();
 };
 
 } // namespace vw
