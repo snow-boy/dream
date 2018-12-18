2b33db4d6fc87e3139aa4a19579d4e337bd76116
1589221904@qq.com <xuesheng.zhang>
2018-12-19 01:04:38 +0800

    完成life的编写

--- src/runtime/life/include/life/life.h
+++ src/runtime/life/include/life/life.h
@@ -3,6 +3,9 @@
 
 #include "life_global.h"
 #include <QObject>
+#include <QList>
+#include <memory>
+#include "scene.h"
 
 namespace dream
 {
@@ -12,6 +15,23 @@ class LIFE_DECL Life: public QObject
     Q_OBJECT
 public:
     Life();
+	~Life();
+
+    Scene *addScene(const QString &name);
+    void removeScene(Scene *scene);
+    QList<Scene *> sceneList();
+
+    void setCurrentScene(Scene *scene);
+    Scene *currentScene();
+
+signals:
+    void sceneAdded(Scene *);
+    void sceneRemoved(Scene *);
+    void currentSceneChanged(Scene *);
+
+private:
+	class Imp;
+	std::unique_ptr<Imp> imp_;
 };
 
 } // namespace dream
--- src/runtime/life/src/life.cpp
+++ src/runtime/life/src/life.cpp
@@ -3,9 +3,60 @@
 namespace dream
 {
 
+class Life::Imp
+{
+public:
+    Imp():
+        current_scene(nullptr)
+    {}
+
+    QList<Scene *> scene_list;
+    Scene *current_scene;
+};
+
 Life::Life()
 {
+    imp_ = std::make_unique<Imp>();
+}
 
+Life::~Life()
+{
+
+}
+
+Scene *Life::addScene(const QString &name)
+{
+    Scene *scene = new Scene(this);
+    scene->setObjectName(name);
+    imp_->scene_list.append(scene);
+    emit sceneAdded(scene);
+    return scene;
+}
+
+void Life::removeScene(Scene *scene)
+{
+    Q_ASSERT(imp_->scene_list.contains(scene));
+    imp_->scene_list.removeOne(scene);
+    emit sceneRemoved(scene);
+    scene->deleteLater();
+}
+
+QList<Scene *> Life::sceneList()
+{
+    return imp_->scene_list;
+}
+
+void Life::setCurrentScene(Scene *scene)
+{
+    if(scene != imp_->current_scene){
+        imp_->current_scene = scene;
+        emit currentSceneChanged(scene);
+    }
+}
+
+Scene *Life::currentScene()
+{
+    return imp_->current_scene;
 }
 
 } //namespace dream
